bf40bc48da4d4783f73d00abc5be847c01c2d543
rasddn <rasddn@telesoftas.com>
2019-06-10 21:22:37 +0300

    Add approval (golden master) test and txt file with updated items output

--- src/test/java/com/rasa/gildedrose/GildedRoseTest.java
+++ src/test/java/com/rasa/gildedrose/GildedRoseTest.java
@@ -1,17 +1,99 @@
 package com.rasa.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.util.Random;
+
+import static org.approvaltests.Approvals.verify;
+
 
 public class GildedRoseTest {
 
+    private static final int SEED = 100;
+    private static final int DAYS_NUMBER = 100;
+    private static final int ITEMS_NUMBER = 20;
+    private static final int QUALITY_MIN = 0;
+    private static final int QUALITY_MAX = 50;
+    private static final int SELLIN_MIN = -10;
+    private static final int SELLIN_MAX = 20;
+    private static final String ITEM_DEXTERY_NAME = "+5 Dexterity Vest";
+    private static final String ITEM_AGED_BRIE_NAME = "Aged Brie";
+    private static final String ITEM_ELIXIR_NAME = "Elixir of the Mongoose";
+    private static final String ITEM_SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    private static final String ITEM_BACKSTAGE_NAME = "Backstage passes to a TAFKAL80ETC concert";
+
+    private static final String[] itemNames = {
+            ITEM_DEXTERY_NAME,
+            ITEM_AGED_BRIE_NAME,
+            ITEM_ELIXIR_NAME,
+            ITEM_SULFURAS_NAME,
+            ITEM_BACKSTAGE_NAME,
+    };
+
+    private Random random = new Random(SEED);
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void
+    generates_updated_items_properties_output() {
+        Item[] items = generateRandomItems();
+        GildedRose gildedRose = new GildedRose(items);
+        verify(getAllDaysItemsStringRepresentation(items, gildedRose));
     }
 
+    private String getAllDaysItemsStringRepresentation(Item[] items, GildedRose gildedRose) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < DAYS_NUMBER; i++) {
+            String header = "-------- day " + i + " --------\nname, sellIn, quality\n";
+            builder.
+                    append(header).
+                    append(getItemsStringRepresentation(items)).
+                    append("\n\n");
+            gildedRose.updateQuality();
+        }
+
+        return builder.toString();
+    }
+
+    private StringBuilder getItemsStringRepresentation(Item[] items) {
+        StringBuilder builder = new StringBuilder();
+        for (Item item : items) {
+            builder.append(item).append("\n");
+        }
+
+        return builder;
+    }
+
+    private Item[] generateRandomItems() {
+        Item[] items = new Item[ITEMS_NUMBER];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = new Item(getItemName(), generateSellIn(), generateQuality());
+            setSpecificItemsQualities(items[i]);
+        }
+
+        return items;
+    }
+
+    private void setSpecificItemsQualities(Item item) {
+        if (item.name.equals(ITEM_SULFURAS_NAME)) {
+            item.quality = 80;
+        } else if (item.name.equals(ITEM_BACKSTAGE_NAME) && item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private String getItemName() {
+        return itemNames[random.nextInt(itemNames.length)];
+    }
+
+    private int generateSellIn() {
+        return generateRandomNumberBetween(SELLIN_MIN, SELLIN_MAX);
+    }
+
+    private int generateQuality() {
+        return generateRandomNumberBetween(QUALITY_MIN, QUALITY_MAX);
+    }
+
+    private int generateRandomNumberBetween(int min, int max) {
+        return min + random.nextInt(max - min + 1);
+    }
 }
