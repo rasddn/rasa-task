ec0d3be1ea67afb84c502a92d0f07189368dcb14
rasddn <rasddn@telesoftas.com>
2019-06-10 23:52:10 +0300

    Add quality min and max constants, also add update sellIn default method to Updatable interface

--- src/main/java/com/rasa/gildedrose/processor/Updatable.java
+++ src/main/java/com/rasa/gildedrose/processor/Updatable.java
@@ -4,5 +4,12 @@ package com.rasa.gildedrose.processor;
 import com.rasa.gildedrose.entity.Item;
 
 public interface Updatable {
+    int QUALITY_MIN = 0;
+    int QUALITY_MAX = 50;
+
     void update(Item item);
+
+    default void updateSellIn(Item item) {
+        item.decrementSellInBy(1);
+    }
 }
