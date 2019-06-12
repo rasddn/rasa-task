7dfc70ed9514d34647ad40e855c89cfbdcadc776
rasddn <rasddn@telesoftas.com>
2019-06-12 22:22:13 +0300

    Enable mongo and scheduling

--- src/main/java/com/rasa/gildedrose/GildedroseApplication.java
+++ src/main/java/com/rasa/gildedrose/GildedroseApplication.java
@@ -1,13 +1,18 @@
 package com.rasa.gildedrose;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableScheduling
 public class GildedroseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GildedroseApplication.class, args);
+        run(GildedroseApplication.class, args);
     }
 
 }
